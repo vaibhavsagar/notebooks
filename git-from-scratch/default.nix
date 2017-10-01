4e76c824e00395c5ad926a71b22dafcdeb0d5102
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-10-01 15:06:07 +0800

    Filter source

--- git-from-scratch/default.nix
+++ git-from-scratch/default.nix
@@ -13,8 +13,20 @@ let
     sha256 = "18d8rcnhb70qqaqa5h2ganazn07a8mimpj29ilbzx0pi5a8zj7bv";
   };
   nixpkgs = import pinned {};
+  cleanSource = name: type: let
+      baseName = baseNameOf (toString name);
+      lib = nixpkgs.lib;
+    in !(
+      (type == "directory" && (baseName == ".git" || baseName == "dist")) ||
+      (type == "directory" && (baseName == "solarized"))                  ||
+      (type == "symlink"   && (lib.hasPrefix "result" baseName))          ||
+      lib.hasSuffix ".hi"    baseName                                     ||
+      lib.hasSuffix ".o"     baseName                                     ||
+      lib.hasSuffix ".ipynb" baseName                                     ||
+      lib.hasSuffix ".sock"  baseName
+    );
   git-from-scratch =
-    nixpkgs.haskellPackages.callCabal2nix "git-from-scratch" (nixpkgs.lib.cleanSource ./.) {};
+    nixpkgs.haskellPackages.callCabal2nix "git-from-scratch" (builtins.filterSource cleanSource ./.) {};
 in import "${IHaskell}/release.nix" {
   pkgs = nixpkgs;
   packages = self: with self; [
