850ff644cf51ea222cb0997aea8ce2aec64c4339
Vaibhav Sagar <vaibhavsagar@gmail.com>
2017-10-27 23:16:44 +0800

    Use updater

--- git-from-scratch/default.nix
+++ git-from-scratch/default.nix
@@ -1,17 +1,9 @@
 let
   fetch    = (import <nixpkgs> {}).fetchFromGitHub;
-  IHaskell = fetch {
-    owner  = "gibiansky";
-    repo   = "IHaskell";
-    rev    = "32aea170c41b181e34e76b9d9eb391e540fed6e1";
-    sha256 = "17n24pxizifvsxyh4lc8jgr45csbmrz936qssj8i5l96yr2fqz4b";
-  };
-  pinned   = fetch {
-    owner  = "NixOS";
-    repo   = "nixpkgs";
-    rev    = "c99239bca08d12bf98000961912b4c0ad52a8a7e";
-    sha256 = "1d3hwaflsyb6vj2czj3jpaxvdmsr448sd0536lhaillvsm087y0g";
-  };
+  # ./updater gibiansky IHaskell > ./ihaskell.json
+  IHaskell = fetch (builtins.fromJSON (builtins.readFile ./ihaskell.json));
+  # ./updater NixOS nixpkgs-channels nixos-17.09 > ./nixpkgs.json
+  pinned   = fetch (builtins.fromJSON (builtins.readFile ./nixpkgs.json));
   nixpkgs = import pinned {};
   cleanSource = name: type: let
       baseName = baseNameOf (toString name);
