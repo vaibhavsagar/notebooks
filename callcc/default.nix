bb9d66bca3e4f919820f8db1de33f7fd74a20f6b
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-01-02 23:37:41 +0800

    callcc: init

---
+++ callcc/default.nix
@@ -0,0 +1,10 @@
+let
+  fetch    = (import <nixpkgs> {}).fetchFromGitHub;
+  # ./updater versions.json ihaskell
+  IHaskell = fetch (builtins.fromJSON (builtins.readFile ./versions.json)).ihaskell;
+  # ./updater versions.json nixpkgs nixos-17.09
+  pinned   = fetch (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs;
+  nixpkgs = import pinned {};
+in import "${IHaskell}/release.nix" {
+  inherit nixpkgs;
+}
