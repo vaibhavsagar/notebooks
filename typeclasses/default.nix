1be70e9be5ae879fb88f0475765f9e5df128e8db
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-02-04 00:41:21 +0800

    typeclasses: simplify default.nix

--- typeclasses/default.nix
+++ typeclasses/default.nix
@@ -5,21 +5,7 @@ let
   # ./updater NixOS nixpkgs-channels nixos-17.09 > ./nixpkgs.json
   pinned   = fetch (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs;
   nixpkgs = import pinned {};
-  cleanSource = name: type: let
-      baseName = baseNameOf (toString name);
-      lib = nixpkgs.lib;
-    in !(
-      (type == "directory" && (baseName == ".git" || baseName == "dist")) ||
-      (type == "directory" && (baseName == "solarized"))                  ||
-      (type == "symlink"   && (lib.hasPrefix "result" baseName))          ||
-      lib.hasSuffix ".nix"   baseName                                     ||
-      lib.hasSuffix ".yaml"  baseName                                     ||
-      lib.hasSuffix ".hi"    baseName                                     ||
-      lib.hasSuffix ".o"     baseName                                     ||
-      lib.hasSuffix ".ipynb" baseName                                     ||
-      lib.hasSuffix ".sock"  baseName
-    );
 in import "${IHaskell}/release.nix" {
   inherit nixpkgs;
-  packages = self: with self; [];
+  packages = self: with self; [ transformers ];
 }
