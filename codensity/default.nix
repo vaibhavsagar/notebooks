6371e0793664cf4755c5f9d7495164edb206becf
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-02-04 19:34:21 +0800

    codensity: init

---
+++ codensity/default.nix
@@ -0,0 +1,11 @@
+let
+  fetch    = (import <nixpkgs> {}).fetchFromGitHub;
+  # ./updater gibiansky IHaskell > ./ihaskell.json
+  IHaskell = fetch (builtins.fromJSON (builtins.readFile ./versions.json)).ihaskell;
+  # ./updater NixOS nixpkgs-channels nixos-17.09 > ./nixpkgs.json
+  pinned   = fetch (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs;
+  nixpkgs = import pinned {};
+in import "${IHaskell}/release.nix" {
+  inherit nixpkgs;
+  packages = self: with self; [];
+}
