2416e6841b590f7b991b3e8c80469f322a973e46
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-02-23 03:16:32 +0800

    Move updater and versions.json to toplevel

--- callcc/default.nix
+++ callcc/default.nix
@@ -1,9 +1,9 @@
 let
   fetch    = (import <nixpkgs> {}).fetchFromGitHub;
   # ./updater versions.json ihaskell
-  IHaskell = fetch (builtins.fromJSON (builtins.readFile ./versions.json)).ihaskell;
+  IHaskell = fetch (builtins.fromJSON (builtins.readFile ../versions.json)).ihaskell;
   # ./updater versions.json nixpkgs nixos-17.09
-  pinned   = fetch (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs;
+  pinned   = fetch (builtins.fromJSON (builtins.readFile ../versions.json)).nixpkgs;
   nixpkgs = import pinned {};
 in import "${IHaskell}/release.nix" {
   inherit nixpkgs;
--- revisiting-monadic-parsing-haskell/default.nix
+++ revisiting-monadic-parsing-haskell/default.nix
@@ -1,9 +1,9 @@
 let
   fetch    = (import <nixpkgs> {}).fetchFromGitHub;
   # ./updater gibiansky IHaskell > ./ihaskell.json
-  IHaskell = fetch (builtins.fromJSON (builtins.readFile ./versions.json)).ihaskell;
+  IHaskell = fetch (builtins.fromJSON (builtins.readFile ../versions.json)).ihaskell;
   # ./updater NixOS nixpkgs-channels nixos-17.09 > ./nixpkgs.json
-  pinned   = fetch (builtins.fromJSON (builtins.readFile ./versions.json)).nixpkgs;
+  pinned   = fetch (builtins.fromJSON (builtins.readFile ../versions.json)).nixpkgs;
   nixpkgs = import pinned {};
 in import "${IHaskell}/release.nix" {
   inherit nixpkgs;
