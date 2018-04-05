610efe0eb38d79727067e92a792caae129432c9d
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-04-05 14:35:53 +0800

    Update comments for 18.03

--- callcc/default.nix
+++ callcc/default.nix
@@ -2,7 +2,7 @@ let
   fetch    = (import <nixpkgs> {}).fetchFromGitHub;
   # ./updater versions.json ihaskell
   IHaskell = fetch (builtins.fromJSON (builtins.readFile ../versions.json)).ihaskell;
-  # ./updater versions.json nixpkgs nixos-17.09
+  # ./updater versions.json nixpkgs nixos-18.03
   pinned   = fetch (builtins.fromJSON (builtins.readFile ../versions.json)).nixpkgs;
   nixpkgs = import pinned {};
 in import "${IHaskell}/release.nix" {
--- typeclasses/default.nix
+++ typeclasses/default.nix
@@ -2,7 +2,7 @@ let
   fetch    = (import <nixpkgs> {}).fetchFromGitHub;
   # ./updater gibiansky IHaskell > ./ihaskell.json
   IHaskell = fetch (builtins.fromJSON (builtins.readFile ../versions.json)).ihaskell;
-  # ./updater NixOS nixpkgs-channels nixos-17.09 > ./nixpkgs.json
+  # ./updater NixOS nixpkgs-channels nixos-18.03 > ./nixpkgs.json
   pinned   = fetch (builtins.fromJSON (builtins.readFile ../versions.json)).nixpkgs;
   nixpkgs = import pinned {};
 in import "${IHaskell}/release.nix" {
