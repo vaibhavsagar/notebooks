251e737b6be99889ffc6ce5200470d18e5b5aa13
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-04-11 11:23:04 +0800

    pkgs.nix: init

---
+++ pkgs.nix
@@ -0,0 +1,10 @@
+let
+  inherit (import <nixpkgs> {}) lib;
+  fetcher = { owner, repo, rev, sha256 }: builtins.fetchTarball {
+    inherit sha256;
+    url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+  };
+  versions = lib.mapAttrs
+    (_: fetcher)
+    (builtins.fromJSON (builtins.readFile ./versions.json));
+in versions
