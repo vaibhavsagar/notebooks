4a48d2c18c0c2d7d02fb57fa0130383d4174a8af
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-06-10 10:36:47 +0200

    trees-that-shrink: init

---
+++ trees-that-shrink/default.nix
@@ -0,0 +1,7 @@
+let
+  pkgs = import ../pkgs.nix;
+in import "${pkgs.ihaskell}/release.nix" {
+  compiler = "ghc822";
+  nixpkgs = import pkgs.nixpkgs {};
+  packages = self: with self; [ containers ];
+}
