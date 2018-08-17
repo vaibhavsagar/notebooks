9510f6c9f35d6032dcea5d99c450a8fc7aa8993d
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-08-17 21:34:18 +0800

    hs-updater: init

---
+++ hs-updater/default.nix
@@ -0,0 +1,19 @@
+let
+  pkgs = import ../pkgs.nix;
+in import "${pkgs.ihaskell}/release.nix" {
+  compiler = "ghc822";
+  nixpkgs  = import pkgs.nixpkgs {};
+  packages = self: with self; [
+    aeson
+    aeson-pretty
+    bytestring
+    containers
+    data-default-class
+    lens-aeson
+    req
+    text
+    transformers
+    unordered-containers
+    vector
+  ];
+}
