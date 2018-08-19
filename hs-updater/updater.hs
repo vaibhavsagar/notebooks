ccbaa07d3676159b7e814954aa85617f91329a17
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-08-19 19:40:07 +0800

    hs-updater: more refactoring

--- hs-updater/updater.hs
+++ hs-updater/updater.hs
@@ -13,7 +13,7 @@ import Control.Monad.Trans.Maybe
 import Data.Aeson
 import Data.Aeson.Types
 import Data.Aeson.Encode.Pretty
-import Data.ByteString.Lazy (ByteString, readFile, writeFile)
+import Data.ByteString.Lazy (readFile, writeFile)
 import Data.Default.Class
 import Data.Maybe (maybe)
 import Data.Semigroup ((<>))
@@ -30,11 +30,11 @@ import System.Process
 data Project = Project { owner, repo, rev, sha256 :: Text }
     deriving (Generic, FromJSON, ToJSON)
 
-data Opts = Opts { fName :: FilePath, pName, bName :: Text, extract :: Bool }
+data Opts = Opts { fname :: FilePath, pname, bname :: Text, extract :: Bool }
 
 r :: MonadHttp m => Project -> Text -> m (JsonResponse Value)
-r p b = req GET
-    (https "api.github.com" /: "repos" /: owner p /: repo p /: "branches" /: b)
+r Project{ owner, repo } b = req GET
+    (https "api.github.com" /: "repos" /: owner /: repo /: "branches" /: b)
     NoReqBody jsonResponse (header "User-Agent" "vaibhavsagar")
 
 getRev :: Project -> Text -> MaybeT IO Text
@@ -43,25 +43,25 @@ getRev project branch = MaybeT $ do
     pure $ res ^? key "commit" . key "sha" . _String
 
 buildURL :: Project -> Text
-buildURL prj = "https://github.com/" <> path <> ".tar.gz"
-    where path = intercalate "/" [owner prj, repo prj, "archive", rev prj]
+buildURL Project{ owner, repo, rev } = "https://github.com/"
+    <> intercalate "/" [owner, repo, "archive", rev] <> ".tar.gz"
 
 getSha256 :: Text -> Bool -> IO Text
 getSha256 url doUnpack = pack . init <$>
     readProcess "nix-prefetch-url" (["--unpack" | doUnpack ] ++ [unpack url]) ""
 
 modify :: Opts -> MaybeT IO Value
-modify Opts{ fName, pName, bName, extract } = do
-    versions <- MaybeT $ decode <$> readFile fName
-    project <- MaybeT . pure $ parseMaybe parseJSON =<< versions ^? key pName
-    rev' <- getRev project bName
+modify Opts{ fname, pname, bname, extract } = do
+    versions <- MaybeT $ decode <$> readFile fname
+    project <- MaybeT . pure $ parseMaybe parseJSON =<< versions ^? key pname
+    rev' <- getRev project bname
     sha256' <- lift $ getSha256 (buildURL project { rev = rev' }) extract
     let project' = project { rev = rev', sha256 = sha256' }
-    pure $ versions & key pName .~ toJSON project'
+    pure $ versions & key pname .~ toJSON project'
 
 update :: Opts -> IO ()
 update opts = runMaybeT (modify opts) >>= maybe (pure ())
-    (writeFile (fName opts) . encodePretty' defConfig
+    (writeFile (fname opts) . encodePretty' defConfig
         { confIndent = Spaces 2, confCompare = compare })
 
 main :: IO ()
