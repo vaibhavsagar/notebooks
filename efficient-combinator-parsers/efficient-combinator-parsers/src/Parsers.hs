1b0f2e7393e8b493ac19baebf81a2e80eec4180a
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-02-16 18:56:43 -0500

    efficient-combinator-parsers: remove SucCont and XorCont newtypes

--- efficient-combinator-parsers/efficient-combinator-parsers/src/Parsers.hs
+++ efficient-combinator-parsers/efficient-combinator-parsers/src/Parsers.hs
@@ -138,9 +138,7 @@ infixr 4 <!>
 infixr 4 >!<
 (>!<) :: ParserC s t r -> ParserC s t r -> ParserC s t r
 (>!<) (ParserC p1) (ParserC p2) = ParserC $ \sc nc -> Parser $ \ss ->
-    unParser
-        (p1 (\r _ -> sc r nc) (unParser (p2 sc nc) ss))
-        ss
+    unParser (p1 (\r _ -> sc r nc) (unParser (p2 sc nc) ss)) ss
 
 (>!*<) :: ParserC s t r -> ParserC s t [r]
 (>!*<) p = (do
@@ -154,21 +152,19 @@ infixr 4 >!<
 
 newtype CParser s t r = CParser
     { unCParser :: SucCont s t r -> XorCont s t -> AltCont s t -> Parser s t }
-newtype SucCont s t r = SucCont
-    { unSucCont :: r -> XorCont s t -> AltCont s t -> Parser s t }
-newtype XorCont s t = XorCont
-    { unXorCont :: AltCont s t -> ParseResult s t }
+type SucCont s t r = r -> XorCont s t -> AltCont s t -> Parser s t
+type XorCont s t =  AltCont s t -> ParseResult s t
 type AltCont s t = ParseResult s t
 
 cSymbol :: Eq s => s -> CParser s t s
 cSymbol sym = CParser $ \sc xc ac -> Parser $ \ss -> case ss of
-    (s:ss) | s == sym -> unParser (unSucCont sc sym xc ac) ss
-    _                 -> unXorCont xc ac
+    (s:ss) | s == sym -> unParser (sc sym xc ac) ss
+    _                 -> xc ac
 
 cSatisfy :: (s -> Bool) -> CParser s t s
 cSatisfy pred = CParser $ \sc xc ac -> Parser $ \ss -> case ss of
-    (s:ss) | pred s -> unParser (unSucCont sc s xc ac) ss
-    _               -> unXorCont xc ac
+    (s:ss) | pred s -> unParser (sc s xc ac) ss
+    _               -> xc ac
 
 instance Functor (CParser s t) where
     fmap :: (u -> v) -> CParser s t u  -> CParser s t v
@@ -176,48 +172,42 @@ instance Functor (CParser s t) where
 
 instance Applicative (CParser s t) where
     pure :: r -> CParser s t r
-    pure x = CParser $ \(SucCont sc) -> sc x
+    pure x = CParser $ \sc -> sc x
 
     (<*>) :: CParser s t (u -> v) -> CParser s t u -> CParser s t v
     (<*>) = ap
 
 instance Alternative (CParser s t) where
     empty :: CParser s t r
-    empty = CParser $ \sc xc ac -> Parser $ \ss -> (unXorCont xc) ac
+    empty = CParser $ \sc xc ac -> Parser $ \ss -> xc ac
 
     (<|>) :: CParser s t r -> CParser s t r -> CParser s t r
     (<|>) (CParser p1) (CParser p2) = CParser $ \sc xc ac -> Parser $ \ss ->
-        unParser (p1 sc (XorCont id) (unParser (p2 sc xc ac) ss)) ss
+        unParser (p1 sc id (unParser (p2 sc xc ac) ss)) ss
 
 instance Monad (CParser s t) where
     (>>=) :: CParser s t u -> (u -> CParser s t v) -> CParser s t v
     (>>=) (CParser p1) f = CParser $ \sc ->
-        p1 (SucCont $ \t -> unCParser (f t) sc)
+        p1 $ \t -> unCParser (f t) sc
 
 infixr 4 <<!>>
 (<<!>>) :: CParser s t r -> CParser s t r -> CParser s t r
 (<<!>>) (CParser p1) (CParser p2) = CParser $ \sc xc ac -> Parser $ \ss ->
-    unParser
-        (p1
-            (SucCont $ \x xc2 -> unSucCont sc x xc)
-            (XorCont $ \ac3 -> unParser (p2 sc xc ac3) ss)
-            ac)
-        ss
+    unParser (p1 (\x xc2 -> sc x xc) (\ac3 -> unParser (p2 sc xc ac3) ss) ac) ss
 
 cBegin :: CParser s t t -> Parser s t
 cBegin (CParser p) = p
-    (SucCont $ \x xc ac -> Parser $ \ss ->
-        ParseResult ((ss,x):unParseResult (unXorCont xc ac)))
-    (XorCont id)
+    (\x xc ac -> Parser $ \ss -> ParseResult ((ss,x):unParseResult (xc ac)))
+    id
     (ParseResult [])
 
 cut :: CParser s t r -> CParser s t r
-cut (CParser p) = CParser $ \sc xc ac -> p sc (XorCont id) ac
+cut (CParser p) = CParser $ \sc xc ac -> p sc id ac
 
 infixr 6 !>>=
 (!>>=) :: CParser s t u -> (u -> CParser s t v) -> CParser s t v
 (!>>=) (CParser p1) f = CParser $ \sc ->
-    p1 (SucCont $ \t ac2 -> unCParser (f t) sc (XorCont id))
+    p1 (\t ac2 -> unCParser (f t) sc id)
 
 (<<!*>>) :: CParser s t r -> CParser s t [r]
 (<<!*>>) p = (p !>>= \r -> (\rs -> (r:rs)) <$> (<<!*>>) p) <<!>> pure []
@@ -233,8 +223,8 @@ cListP (CParser p) l = clp l
     where
         clp l = CParser $ \sc xc ac -> Parser $ \ss -> unParser
             (p
-                (SucCont $ \r xc2 -> unCParser (clp (r:l)) sc (XorCont id))
-                (XorCont $ \ac4 -> unParser (unSucCont sc (reverse l) xc ac4) ss)
+                (\r xc2 -> unCParser (clp (r:l)) sc id)
+                (\ac4 -> unParser (sc (reverse l) xc ac4) ss)
                 ac)
             ss
 
