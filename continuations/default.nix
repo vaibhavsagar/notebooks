8dba2e82efb76d0bb28dd030a84d6a2cc7ab2246
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-04-20 14:16:10 -0400

    continuations: update default.nix

--- continuations/default.nix
+++ continuations/default.nix
@@ -1,7 +1,7 @@
 let
   pkgs = import ../pkgs.nix;
-in import "${pkgs.ihaskell}/release.nix" {
-  compiler = "ghc822";
-  nixpkgs  = import pkgs.nixpkgs {};
+in import "${pkgs.ihaskell}/release-8.6.nix" {
+  nixpkgs = import pkgs.nixpkgs {};
+  compiler = "ghc864";
   packages = self: with self; [];
 }
