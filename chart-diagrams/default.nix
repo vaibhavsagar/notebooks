d726cecd18b9bbb3ac9031c8b29c152402c7a3ae
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-05-10 13:22:55 -0400

    chart-diagrams: init

---
+++ chart-diagrams/default.nix
@@ -0,0 +1,20 @@
+let
+  pkgs = import ../pkgs.nix;
+  overlay = sel: sup: {
+    haskell = sup.haskell // {
+      packages = sup.haskell.packages // {
+        ghc844 = sup.haskell.packages.ghc844.override {
+          overrides = self: super: {
+            system-fileio = sel.haskell.lib.doJailbreak super.system-fileio;
+            Chart = sel.haskell.lib.doJailbreak super.Chart;
+            Chart-cairo = sel.haskell.lib.doJailbreak super.Chart-cairo;
+          };
+        };
+      };
+    };
+  };
+in import "${pkgs.ihaskell}/release.nix" {
+  nixpkgs = import pkgs.nixpkgs { config.allowBroken = true; overlays = [ overlay ]; };
+  compiler = "ghc844";
+  packages = self: with self; [ ihaskell-charts ihaskell-diagrams ];
+}
