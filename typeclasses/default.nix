bc8b8271236daf3b0d82076a842565d584e41f38
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-06-20 16:37:41 -0400

    typeclasses: update State.ipynb with monad transformer

--- typeclasses/default.nix
+++ typeclasses/default.nix
@@ -1,6 +1,7 @@
 let
   pkgs = import ../pkgs.nix;
 in import "${pkgs.ihaskell}/release.nix" {
+  compiler = "ghc864";
   nixpkgs = import pkgs.nixpkgs {};
   packages = self: with self; [ transformers ];
 }
