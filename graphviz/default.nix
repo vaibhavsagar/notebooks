df4dd88194e4eeab0ec9e2b9b86d307e060a4e82
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-06-21 15:19:54 -0400

    graphviz: use ghc864

--- graphviz/default.nix
+++ graphviz/default.nix
@@ -1,7 +1,7 @@
 let
   pkgs = import ../pkgs.nix;
 in import "${pkgs.ihaskell}/release.nix" {
-  compiler = "ghc844";
+  compiler = "ghc864";
   nixpkgs  = import pkgs.nixpkgs {};
   packages = self: with self; [ here ihaskell-graphviz ];
   systemPackages = self: with self; [ graphviz ];
