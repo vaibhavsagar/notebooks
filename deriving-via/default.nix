e1a0f3c92b87d6689cf3e940947f094259bfad74
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-06-28 17:34:45 -0400

    deriving-via: use builtins.fetchurl

--- deriving-via/default.nix
+++ deriving-via/default.nix
@@ -4,8 +4,8 @@ let
     pname = "rise";
     version = "5.5.0";
     name = "${pname}-${version}";
-    src = (import pkgs.nixpkgs {}).fetchurl {
-      url = "mirror://pypi/${builtins.substring 0 1 pname}/${pname}/${name}.tar.gz";
+    src = builtins.fetchurl {
+      url = "https://files.pythonhosted.org/packages/source/r/rise/rise-5.5.0.tar.gz";
       sha256 = "0885xf5zaf0lj8cxvkwam61n6yfqasy6ri5zzyh6hvdj9pg70mwp";
     };
     propagatedBuildInputs = [ pythonPackages.notebook ];
