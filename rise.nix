06367ad41bce0045083f4e85d2a134f8c684d0f5
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-07-07 22:17:47 -0400

    rise.nix: reduce duplication

--- rise.nix
+++ rise.nix
@@ -3,7 +3,7 @@ pythonPackages: pythonPackages.buildPythonPackage rec {
   version = "5.5.0";
   name = "${pname}-${version}";
   src = builtins.fetchurl {
-    url = "https://files.pythonhosted.org/packages/source/r/rise/rise-5.5.0.tar.gz";
+    url = "https://files.pythonhosted.org/packages/source/r/${pname}/${name}.tar.gz";
     sha256 = "0885xf5zaf0lj8cxvkwam61n6yfqasy6ri5zzyh6hvdj9pg70mwp";
   };
   propagatedBuildInputs = [ pythonPackages.notebook ];
