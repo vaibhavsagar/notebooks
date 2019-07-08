074605ece8d037b5317109864077a40f64ed400c
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-07-07 22:24:46 -0400

    rise.nix: 5.5.0 -> 5.5.1

--- rise.nix
+++ rise.nix
@@ -1,10 +1,10 @@
 pythonPackages: pythonPackages.buildPythonPackage rec {
   pname = "rise";
-  version = "5.5.0";
+  version = "5.5.1";
   name = "${pname}-${version}";
   src = builtins.fetchurl {
     url = "https://files.pythonhosted.org/packages/source/r/${pname}/${name}.tar.gz";
-    sha256 = "0885xf5zaf0lj8cxvkwam61n6yfqasy6ri5zzyh6hvdj9pg70mwp";
+    sha256 = "0lh046s6r9jjs0vd2hqn82prn25mhjajzw9byh5gy1ypf28m4gas";
   };
   propagatedBuildInputs = [ pythonPackages.notebook ];
 }
