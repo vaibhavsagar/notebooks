1d326ee15f271504a8553f39d7e5a1eb37e3a9ad
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-07-12 00:35:44 -0400

    smt: update sbv

--- smt/default.nix
+++ smt/default.nix
@@ -3,27 +3,20 @@ let
   overlay = sel: sup: {
     haskell = sup.haskell // {
       packages = sup.haskell.packages // {
-        ghc844 = sup.haskell.packages.ghc844.override {
+        ghc864 = sup.haskell.packages.ghc864.override {
+          all-cabal-hashes = sel.fetchurl {
+            url = "https://github.com/commercialhaskell/all-cabal-hashes/archive/09dd3a69af0d88e0a8df6552b2398185a8740abf.tar.gz";
+            sha256 = "1xs6cc8zz5lff1bvzwn1sw076fislpxfk7zsyvwfp5hgl46bjhj6";
+          };
           overrides = self: super: {
-            crackNum = self.callCabal2nix "crackNum" (sel.fetchFromGitHub {
-              owner = "LeventErkok";
-              repo = "crackNum";
-              rev = "54cf70861a921062db762b3c50e933e73446c3b2";
-              sha256 = "02cg64rq8xk7x53ziidljyv3gsshdpgbzy7h03r869gj02l7bxwa";
-            }) {};
-            sbv = self.callCabal2nix "sbv" (sel.fetchFromGitHub {
-              owner = "LeventErkok";
-              repo = "sbv";
-              rev = "a2f71a07fa7896bebcc2f91e20eeb00162138e7d";
-              sha256 = "0b6a9lj3ch1iji6kia7jpri87v2jnk68chpshxv54wzld93c2qvb";
-            }) {};
+            sbv = self.callHackage "sbv" "8.3" {};
           };
         };
       };
     };
   };
 in import "${pkgs.ihaskell}/release.nix" {
-  compiler = "ghc844";
+  compiler = "ghc864";
   nixpkgs  = import pkgs.nixpkgs { overlays = [ overlay ]; };
   packages = self: with self; [ sbv ];
   systemPackages = self: with self; [ z3 ];
