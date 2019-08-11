95a7b5171d01551b0cb2196ea20d5df8c99e03dc
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-08-11 18:33:21 -0400

    docker: init

---
+++ docker/default.nix
@@ -0,0 +1,34 @@
+let
+  pkgs = import ../pkgs.nix;
+  nixpkgs = import pkgs.nixpkgs {};
+  NB_USER = "jovyan";
+  NB_UID = "1000";
+  ihaskell = import "${pkgs.ihaskell}/release.nix" {
+    inherit nixpkgs;
+    compiler = "ghc864";
+    packages = self: with self; [];
+  };
+  image = nixpkgs.dockerTools.buildImage {
+    name = "ihaskell-nix";
+    tag = "latest";
+    contents =  [
+      ihaskell
+      nixpkgs.bashInteractive
+      nixpkgs.coreutils
+    ];
+    runAsRoot = ''
+      #!${nixpkgs.runtimeShell}
+      ${nixpkgs.dockerTools.shadowSetup}
+      mkdir -p /tmp
+      chmod 1777 /tmp
+      mkdir -p /home/${NB_USER}
+      ${nixpkgs.busybox}/bin/adduser --disabled-password --gecos "Default user" --uid ${NB_UID} ${NB_USER}
+      chown -R ${NB_UID} /home/${NB_USER}
+    '';
+    config = {
+      Cmd = ["ihaskell-notebook" "--ip" "0.0.0.0"];
+      User = NB_USER;
+      WorkingDir = "/home/${NB_USER}";
+    };
+  };
+in image
