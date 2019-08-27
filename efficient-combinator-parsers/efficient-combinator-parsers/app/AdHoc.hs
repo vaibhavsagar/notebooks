7a7ff07cdaea986caea14295321df6e2d11d5a11
Vaibhav Sagar <vaibhavsagar@gmail.com>
2019-08-27 11:12:45 -0400

    efficient-combinator-parsers: remove Parsers module

--- efficient-combinator-parsers/efficient-combinator-parsers/app/AdHoc.hs
+++ efficient-combinator-parsers/efficient-combinator-parsers/app/AdHoc.hs
@@ -1,6 +1,7 @@
 module Main where
 
-import Parsers
+import Data.Char
+import Control.Applicative(Alternative(..))
 import System.Environment
 
 main :: IO ()
@@ -12,3 +13,16 @@ main = do
             print $ length $ snd $ head (manParse contents)
         _ -> error "filename not provided"
 
+manParse :: String -> [(String, [String])]
+manParse input = word [] [] input
+    where
+        word w s (c:r) | isAlpha c = word (c:w) s r
+        word w s input             = sep [] ((reverse w):s) input
+
+        sep l  s (c:r) | isSpace c || c == ',' = sep (c:l) s r
+        sep [] s input                         = dot s input
+        sep _  s input                         = word [] s input
+
+        dot [] input   = []
+        dot s  ('.':r) = [(r,reverse s)]
+        dot _  _       = []
