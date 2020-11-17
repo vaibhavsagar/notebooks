3e59138bc3c7fb875304908a2efbed03949b3d6d
Vaibhav Sagar <vaibhavsagar@gmail.com>
2020-11-17 23:03:52 +1100

    mph: bump to ghc884

--- mph/default.nix
+++ mph/default.nix
@@ -1,7 +1,7 @@
 let
   pkgs = import ../pkgs.nix;
 in import "${pkgs.ihaskell}/release.nix" {
-  compiler = "ghc865";
+  compiler = "ghc884";
   nixpkgs  = import pkgs.nixpkgs {};
   packages = self: with self; [ bv-little unordered-containers ];
 }
