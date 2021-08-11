0595b795e3750ac5eead035ddb37c573e1d2b0ec
Vaibhav Sagar <vaibhavsagar@gmail.com>
2021-08-12 01:41:05 +1000

    default.nix: update

--- chart-diagrams/default.nix
+++ chart-diagrams/default.nix
@@ -2,6 +2,6 @@ let
   pkgs = import ../pkgs.nix;
 in import "${pkgs.ihaskell}/release.nix" {
   nixpkgs = import pkgs.nixpkgs {};
-  compiler = "ghc865";
+  compiler = "ghc8104";
   packages = self: with self; [ ihaskell-aeson ihaskell-blaze ihaskell-charts ihaskell-diagrams ];
 }
