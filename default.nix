38b954fd633641f7f9ec89ebde5f84074d835bbe
Vaibhav Sagar <vaibhavsagar@gmail.com>
2021-08-27 20:18:44 +1000

    default.nix: init

---
+++ default.nix
@@ -0,0 +1,35 @@
+let
+  pkgs    = import ./pkgs.nix;
+  nixpkgs = import pkgs.nixpkgs {};
+  notebooks = map (folder: {
+    name = folder;
+    path = import (./. + "/${folder}");
+  });
+in nixpkgs.linkFarm "notebooks" (notebooks [
+  "callcc"
+  "chart-diagrams"
+  "codensity"
+  "continuations"
+  "deriving-via"
+  "docker"
+  "dragon-curve"
+  "efficient-combinator-parsers"
+  "git-from-scratch"
+  "graphviz"
+  "hamt"
+  "hs-updater"
+  "inline-r"
+  "intmap"
+  "lambda"
+  "llvm"
+  "mph"
+  "refactoring-tarjan"
+  "revisiting-monadic-parsing-haskell"
+  "revisiting-poor-mans-concurrency"
+  "smt"
+  "solver"
+  "tarjan"
+  "trees-that-shrink"
+  "typeclasses"
+  "zulip-api"
+])
