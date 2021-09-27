c4200cf4c705e6a6c10c02657ae186dce014d58f
Vaibhav Sagar <vaibhavsagar@gmail.com>
2021-09-27 17:07:58 +1000

    Revert "hamt: using '--enable-executable-dynamic' makes full restarts faster"
    This reverts commit 99c73a9d8d1f0f7f50bc985452b52f6d069b8285.

--- hamt/default.nix
+++ hamt/default.nix
@@ -1,21 +1,8 @@
 let
   pkgs = import ../pkgs.nix;
+in import "${pkgs.ihaskell}/release.nix" {
+  compiler = "ghc8104";
   nixpkgs  = import pkgs.nixpkgs {};
-  ihaskell = import "${pkgs.ihaskell}/release.nix" {
-    inherit nixpkgs;
-    compiler = "ghc8104";
-    packages = self: with self; [ bytestring memory pretty-show timeit vector here ihaskell-graphviz ];
-  };
-  customisedIHaskellExe = nixpkgs.haskell.lib.overrideCabal ihaskell.passthru.haskellPackages.ihaskell (_drv: {
-    enableSharedExecutables = true;
-  });
-  customisedIHaskell = with ihaskell.passthru; ihaskellBuildEnvFunc {
-    inherit ihaskellEnv jupyterlab;
-    systemPackages = self: with self; [ graphviz ];
-    ihaskellDataDir = let
-      ihaskellGhcLib =  ihaskellGhcLibFunc customisedIHaskellExe ihaskellEnv;
-      ihaskellKernelFile = ihaskellKernelFileFunc ihaskellGhcLib "-M3g -N2";
-      ihaskellKernelSpec = ihaskellKernelSpecFunc ihaskellKernelFile;
-    in ihaskellDataDirFunc ihaskellKernelSpec ihaskellLabextension;
-  };
-in customisedIHaskell
+  packages = self: with self; [ bytestring memory pretty-show timeit vector here ihaskell-graphviz ];
+  systemPackages = self: with self; [ graphviz ];
+}
