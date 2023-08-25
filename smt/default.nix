99d4e2f2853a5eefb96fe4cbe0ac5d46707f0dde
Vaibhav Sagar <vaibhavsagar@gmail.com>
2023-08-25 20:14:17 +1000

    smt: update

--- smt/default.nix
+++ smt/default.nix
@@ -1,8 +1,19 @@
 let
   pkgs = import ../pkgs.nix;
+  overlay = sel: sup: {
+    haskell = sup.haskell // {
+      packages = sup.haskell.packages // {
+        ghc928 = sup.haskell.packages.ghc928.override {
+          overrides = self: super: {
+            sbv = sup.haskell.lib.dontCheck (self.callHackage "sbv" "10.1" {});
+          };
+        };
+      };
+    };
+  };
 in import "${pkgs.ihaskell}/release.nix" {
   compiler = "ghc928";
-  nixpkgs  = import pkgs.nixpkgs {};
+  nixpkgs  = import pkgs.nixpkgs { overlays = [ overlay ]; };
   packages = self: with self; [ sbv ];
   systemPackages = self: with self; [ z3 ];
 }
