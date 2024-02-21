e0dab49556b23ad13af8ec548bdae1aa8060c69d
Vaibhav Sagar <vaibhavsagar@gmail.com>
2024-02-21 21:30:39 +1100

    flake.nix: use legacyPackages

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
 
   outputs = {self, nixpkgs, flake-utils, ...}:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = nixpkgs.legacyPackages.${system};
       notebook = folder: {
         name = folder;
         path = import (./. + "/${folder}") { inherit system; };
