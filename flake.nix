2e767925240de9ac3e411e80810cc436fd9e7aef
Vaibhav Sagar <vaibhavsagar@gmail.com>
2024-12-02 09:27:11 +1100

    Use Nixpkgs 24.11

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nix-filter.url = "github:numtide/nix-filter";
   inputs.ihaskell.url = "github:IHaskell/IHaskell";
